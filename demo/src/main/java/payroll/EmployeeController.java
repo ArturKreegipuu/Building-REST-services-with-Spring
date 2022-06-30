cd32a5ae526138a3c6997410c624c2b5fedefff4
Artur Kreegipuu <artur@kreegipuu.net>
2022-06-30 14:25:59 +0300

    esialgne restcontroller valmis

--- demo/src/main/java/payroll/EmployeeController.java
+++ demo/src/main/java/payroll/EmployeeController.java
@@ -17,20 +17,21 @@ class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
-        this.repository = repository;
-    }
+    private final EmployeeModelAssembler assembler;
 
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+
+        this.repository = repository;
+        this.assembler = assembler;
+    }
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
 
-        List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -50,9 +51,7 @@ class EmployeeController {
         Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
